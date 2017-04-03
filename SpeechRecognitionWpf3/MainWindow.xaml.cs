746ec188cf7da15600ce2e8fc0638bf26a6d8bb4
tichinose1 <toyoh.ichinose@unisys.co.jp>
2017-04-03 10:47:51 +0900

    working?

---
+++ SpeechRecognitionWpf3/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpeechRecognitionWpf3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            OxfordClient.OnPartialResponseReceived += text =>
+            {
+                Dispatcher.InvokeAsync(() =>
+                {
+                    InputTextBox.Text = text;
+                });
+            };
+
+            OxfordClient.OnResponseReceived += text =>
+            {
+                Dispatcher.InvokeAsync(() =>
+                {
+                    InputTextBox.IsEnabled = true;
+                });
+            };
+
+            OxfordClient.Initialize();
+
+            InputTextBox.IsEnabled = false;
+            OxfordClient.Start();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            InputTextBox.IsEnabled = false;
+            OxfordClient.Start();
+        }
+    }
+}
---
+++ SpeechRecognitionWpf3/OxfordClient.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.CognitiveServices.SpeechRecognition;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeechRecognitionWpf3
+{
+    public static class OxfordClient
+    {
+        public static Action<string> OnPartialResponseReceived;
+        public static Action<string> OnResponseReceived;
+
+        private static MicrophoneRecognitionClient client;
+
+        public static void Initialize()
+        {
+            client = SpeechRecognitionServiceFactory.CreateMicrophoneClient(
+                SpeechRecognitionMode.ShortPhrase,
+                "ja-JP",
+                "90208e524a694073908643976ff7efeb");
+
+            client.OnResponseReceived += Client_OnResponseReceived;
+            client.OnPartialResponseReceived += Client_OnPartialResponseReceived;
+        }
+
+        public static void Start()
+        {
+            client.StartMicAndRecognition();
+        }
+
+        private static void Client_OnPartialResponseReceived(object sender, PartialSpeechResponseEventArgs e)
+        {
+            OnPartialResponseReceived?.Invoke(e.PartialResult);
+            Debug.WriteLine("OnPartialResponseReceived: " + e.PartialResult);
+        }
+
+        private static void Client_OnResponseReceived(object sender, SpeechResponseEventArgs e)
+        {
+            if (e.PhraseResponse.Results.Length == 0) return;
+
+            // 不要？
+            //client.EndMicAndRecognition();
+
+            foreach (var item in e.PhraseResponse.Results)
+            {
+                Debug.WriteLine("OnResponseReceived: " + item.DisplayText);
+            }
+
+            OnResponseReceived?.Invoke(e.PhraseResponse.Results[0].DisplayText);
+        }
+    }
+}
